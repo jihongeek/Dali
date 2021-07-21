797cb6af12cd9ef2594d51a5e86c5c80fcd384da
jihongeek <jihongeek@kakao.com>
2021-07-21 12:58:23 +0900

    change dali to post proper markdown file according to time

--- dali.py
+++ dali.py
@@ -1,20 +1,30 @@
 import datetime
 from pytz import timezone
+from glob import glob
 import os,requests
+
+token = os.getenv('AGIT_TOKEN')
+
+feedbackWeekday = int(os.getenv('FB_WEEKDAY'))
+feedbackHour = int(os.getenv('FB_HOUR'))
+feedbackMinute = int(os.getenv('FB_MINUTE'))
 
 weekdays = [0,1,2,3,4,5,6]
 nowTime = datetime.datetime.now(timezone("Asia/Seoul"))
 nowTime = datetime.datetime(nowTime.year,nowTime.month,nowTime.day,nowTime.hour,nowTime.minute,0)
 
-feedbackTime = nowTime + datetime.timedelta(days = weekdays[1-nowTime.weekday()]) 
-feedbackTime = datetime.datetime(feedbackTime.year,feedbackTime.month,feedbackTime.day,15,47)
+feedbackTime = nowTime + datetime.timedelta(days = weekdays[feedbackWeekday-nowTime.weekday()]) 
+feedbackTime = datetime.datetime(feedbackTime.year,feedbackTime.month,feedbackTime.day,feedbackHour,feedbackMinute)
+feedbackDelta = feedbackTime - nowTime
 
-if feedbackTime == nowTime + datetime.timedelta(minutes=3):
-    postFile = open("posts/feedback-0-0-3.md",mode = "r",encoding="UTF-8")
-    post = {"text" : postFile.read()}
-    requests.post("https://agit.io/webhook/"+token,post)
-
-
+os.chdir("posts")
+posts =  glob("*.md")
+post =f"feedback-{feedbackDelta.days}-{feedbackDelta.seconds//3600}-{(feedbackDelta.seconds%3600)//60}.md" 
+if post in posts:
+    print("ok")
+    with open(post,mode = "r",encoding="UTF-8") as postFile:
+        postContent = {"text" : postFile.read()}
+        requests.post("https://agit.io/webhook/"+token,postContent)
 
 
 
